7ab6f406094731c1eec926e10f19cb1c98cac2b7
iyu0805 <t110360142@ntut.org.tw>
2023-10-12 10:26:55 +0800

    first

---
+++ src/hwCH1_110360142/Sample4.java
@@ -0,0 +1,12 @@
+package hwCH1_110360142;
+
+public class Sample4 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("顯示出反斜線:\\");
+		System.out.println("顯示出單引號:\'");
+	}
+
+}
